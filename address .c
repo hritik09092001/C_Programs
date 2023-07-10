75af8daf473f48d4adf5231d0894636d77411751
Hritik Srivastava <122910598+hritik09092001@users.noreply.github.com>
2023-07-10 22:28:03 +0530

    Create address .c

---
+++ address .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str3[100],str2[50],str[50],str4[50];
+puts("Enter Your Name");
+gets(str2);
+puts("Enter City Name");
+gets(str);
+puts("House No");
+gets(str3);
+puts("Enter area");
+gets(str4);
+puts(str2);
+puts(str);
+puts(str4);
+puts(str3);
+
+
+getch();
+}
