654fde82b07b8360681db80ceda385a1290e400e
Hritik Srivastava <122910598+hritik09092001@users.noreply.github.com>
2023-07-12 22:38:17 +0530

    Create table .c

---
+++ table .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,n1;
+clrscr();
+for(i=1;i<=10;i++)
+{
+for(j=1;j<=10;j++)
+{
+n1=i*j;
+printf("%d ",n1);
+}
+printf("\n");
+}
+getch();
+
+}
