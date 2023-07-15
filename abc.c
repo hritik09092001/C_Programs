10e7ab61aebbb5a1dbb3229da2b8b6b7df811c4d
Hritik Srivastava <122910598+hritik09092001@users.noreply.github.com>
2023-07-15 21:42:55 +0530

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+int a,b,c;
+
+printf("Enter the value of the a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+printf("%dx +%dy=%d",a,b,c);
+getch();
+
+}
